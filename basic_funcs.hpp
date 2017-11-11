7b1763bc9d79f88db99ac2f882180987c7433205
owenyoung75 <yangtong@YangTongs-MacBook-Pro.local>
2017-11-11 15:16:44 -0500

    template_version
    with all funcs in headers now

--- basic_funcs.hpp
+++ basic_funcs.hpp
@@ -7,29 +7,38 @@
 //
 #include <vector>
 #include <stdio.h>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/matrix_proxy.hpp>
+#include <boost/numeric/ublas/io.hpp>
 
 #ifndef basic_functions_hpp
 #define basic_functions_hpp
 
 
 namespace STAT_TEST {
-    
+    using namespace boost::numeric::ublas;
+
+    // calculate MEAN of a std::vector
     template<typename _num_type>
-    inline double Mean_value(std::vector<_num_type> _data_group)
+    inline double Mean_value(std::vector<_num_type> _data)
     {
-        int size_group = _data_group.size();
-        
-        double sum = 0.0;
-        for (int i = 0; i<size_group; i++)
-            sum += _data_group[i];
-        
-        return sum/size_group;
+        double rlt = accumulate( _data.begin(), _data.end(), 0.0)/ _data.size();
+        return rlt;
+    }
+    // calculate MEAN of a boost::vector
+    template<typename _num_type>
+    inline double Mean_value(vector<_num_type> _data)
+    {
+        double rlt = accumulate( _data.begin(), _data.end(), 0.0)/ _data.size();
+        return rlt;
     }
  
     
     
+    
+    // calculate VARIANCE of a std::vector
     template<typename _num_type>
-    inline double Variance(std::vector<_num_type> _data_group)
+    inline double Variance_value(std::vector<_num_type> _data_group)
     {
         int size_group = _data_group.size();
         double mean = Mean_value<_num_type>(_data_group);
@@ -40,39 +49,50 @@ namespace STAT_TEST {
         
         return sum/size_group;
     }
-    
-    
-    
+    // calculate VARIANCE of a boost::vector
     template<typename _num_type>
-    inline std::vector<std::vector<double>> Covariance_Matrix
-    (std::vector<std::vector<_num_type>> _data_groups)
+    inline double Variance_value(vector<_num_type> _data_group)
     {
-        int num_of_varbs = _data_groups.size();
-        std::vector<double> means;
-        std::vector<std::vector<double>> Matx;
+        int size_group = _data_group.size();
+        double mean = Mean_value<_num_type>(_data_group);
+        
+        double sum = 0.0;
+        for (int i = 0; i<size_group; i++)
+            sum += (_data_group(i) - mean)*(_data_group(i) - mean);
+        
+        return sum/size_group;
+    }
+    
+    
+    
+    
+    // calculate COVARIANCE of a rectagular boost::matrix
+    // _data_groups = (row_idx, col_idx)
+    // row_idx :: variable 1, 2, 3 ......
+    // col_idx :: copy 1, 2, 3 ...... --> closer address
+    template<typename _num_type>
+    inline matrix<double> Covariance_Matrix (matrix<_num_type> _data_groups)
+    {
+        int num_of_varbs = _data_groups.size1();
+        vector<double> means(num_of_varbs);
+        matrix<double> Matx(num_of_varbs, num_of_varbs);
         
         for (int i = 0; i<num_of_varbs; i++)
         {
-            means[i] = Mean_value<_num_type>(_data_groups[i]);
-            Matx[i][i] = Variance<_num_type>(_data_groups[i]);
+            means(i) = Mean_value<_num_type>( row(_data_groups, i) );
+            Matx(i,i) = Variance_value<_num_type>( row(_data_groups, i) );
         }
-        
+
         for (int i = 0; i<(num_of_varbs-1); i++)
         {
             for (int j = i+1; j<num_of_varbs; j++)
             {
-                int size_group1 = _data_groups[i].size();
-                int size_group2 = _data_groups[j].size();
                 double sum = 0.0;
+                for (int cpy = 0; cpy<_data_groups.size2(); cpy++)
+                    sum += ( _data_groups(i,cpy) - means(i) ) * ( _data_groups(j,cpy) - means(j) );
                 
-                for (int p = 0; p<size_group1; p++)
-                {
-                    for (int q = 0; q<size_group2; j++)
-                        sum += (_data_groups[i] - means[i])*(_data_groups[j] - means[j]);
-                }
-                
-                Matx[i][j] = sum/(size_group1*size_group2);
-                Matx[j][i] = Matx[i][j];
+                Matx(i,j) = sum/(_data_groups.size2());
+                Matx(j,i) = Matx(i,j);
             }
         }
         
